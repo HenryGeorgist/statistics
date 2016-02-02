fd88acc59ff9d696a7c865bd4205959acf50a8d3
HenryGeorgist <willyplehman@gmail.com>
2016-02-01 19:46:25 -0800

    added emperical distribution to support consequence calculations fatality rate distributions (which are emperical)

---
+++ src/Distributions/MethodOfMoments/Emperical.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Distributions.MethodOfMoments;
+
+import Distributions.ContinuousDistributionError;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Will_and_Sara
+ */
+public class Emperical extends Distributions.ContinuousDistribution{
+    private double[] _CumulativeProbabilities;
+    private double[] _ExceedanceValues;
+    @Override
+    public double GetInvCDF(double probability) {
+        int index = java.util.Arrays.binarySearch(_CumulativeProbabilities, probability);
+        //interpolate or step?
+        //check for array out of bounds...
+        return _ExceedanceValues[index];
+    }
+
+    @Override
+    public double GetCDF(double value) {
+        int index = java.util.Arrays.binarySearch(_ExceedanceValues, value);
+        //interpolate or step?
+        return _CumulativeProbabilities[index];
+    }
+
+    @Override
+    public double GetPDF(double value) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ArrayList<ContinuousDistributionError> Validate() {
+        //check for monotonic cumulative probabilities and exceedance values.
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
